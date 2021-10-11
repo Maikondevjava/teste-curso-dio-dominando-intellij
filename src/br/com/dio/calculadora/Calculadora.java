9dff852cf390063090bd8bd51fba1592147a7f66
Maikondevjava <maikondevjava@gmail.com>
2021-10-11 00:31:49 -0300

    Criando package calculadora e class calculadora

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,37 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a, b;
+        System.out.println("Digite o primeiro número:");
+        a = scan.nextInt();
+        System.out.println("Digite o segundo número:");
+        b = scan.nextInt();
+
+        int somar = somar(a, b);
+        int subitrair = subitrair(a, b);
+        int multiplicar = multiplicar(a, b);
+        double dividir = dividir(a, b);
+
+        System.out.println("Somar: "+ somar);
+        System.out.println("Subitrair: "+ subitrair);
+        System.out.println("Multiplicar: "+ multiplicar);
+        System.out.println("Dividir: "+ dividir);
+    }
+
+    public static int somar(int a, int b){
+        return a + b;
+    }
+    public static int subitrair(int a, int b){
+        return a - b;
+    }
+    public static int multiplicar(int a, int b){
+        return a * b;
+    }
+    public static double dividir(double a, double b){
+        return a / b;
+    }
+}
